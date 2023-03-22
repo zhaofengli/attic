2e68228fee5a2ca827fd8ac4501cec2b80a36241
Zhaofeng Li <hello@zhaofeng.li>
2023-03-21 18:45:34 -0600

    token: Enforce the same lint configs

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -63,6 +63,21 @@
 //! }
 //! ```
 
+#![deny(
+    asm_sub_register,
+    deprecated,
+    missing_abi,
+    unsafe_code,
+    unused_macros,
+    unused_must_use,
+    unused_unsafe
+)]
+#![deny(clippy::from_over_into, clippy::needless_question_mark)]
+#![cfg_attr(
+    not(debug_assertions),
+    deny(unused_imports, unused_mut, unused_variables)
+)]
+
 pub mod util;
 
 #[cfg(test)]
