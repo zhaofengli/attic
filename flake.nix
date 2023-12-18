e7a582819253530ee8339b600d79b437f7925ab1
Zhaofeng Li <hello@zhaofeng.li>
2023-12-18 14:50:33 -0700

    flake: Update deps

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     crane = {
