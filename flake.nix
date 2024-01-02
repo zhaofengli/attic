e6bedf1869f382cfc51b69848d6e09d51585ead6
Zhaofeng Li <hello@zhaofeng.li>
2024-01-01 18:34:19 -0700

    flake.nix: Remove non-existent overrides

--- flake.nix
+++ flake.nix
@@ -9,8 +9,6 @@
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-compat.follows = "flake-compat";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     flake-compat = {
