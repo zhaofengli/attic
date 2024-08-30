fcb7c6067f42fa76d2a7cfaba2df8376b354c7ff
Zhaofeng Li <hello@zhaofeng.li>
2024-08-30 12:32:10 -0400

    flake: Bump nixpkgs-stable to 24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     crane = {
