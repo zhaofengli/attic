1bd0279b79a2dd53c11d40be738536d8b71c53bb
Zhaofeng Li <hello@zhaofeng.li>
2024-09-11 10:57:59 -0400

    attic/build.rs: Fix warnings when nix_store is disabled

--- attic/build.rs
+++ attic/build.rs
@@ -2,82 +2,85 @@
 //!
 //! We link against libnixstore to perform actions on the Nix Store.
 
-use cc::Build;
-use version_compare::Version;
-
-struct NixDependency {
-    version: String,
-}
-
-impl NixDependency {
-    fn detect() -> Self {
-        let library = pkg_config::Config::new()
-            .cargo_metadata(false)
-            .atleast_version("2.4")
-            .probe("nix-main")
-            .expect("Failed to find nix-main >=2.4 through pkg-config");
-
-        Self {
-            version: library.version,
-        }
-    }
-
-    fn apply_version_flags(&self, build: &mut Build) {
-        let version = Version::from(&self.version).unwrap();
-
-        if version >= Version::from("2.20").unwrap() {
-            build.flag("-DATTIC_NIX_2_20");
-        }
-    }
-
-    fn emit_cargo_metadata(&self) {
-        pkg_config::Config::new()
-            .atleast_version("2.4")
-            .probe("nix-store")
-            .unwrap();
-
-        pkg_config::Config::new()
-            .atleast_version("2.4")
-            .probe("nix-main")
-            .unwrap();
-    }
-}
-
 fn main() {
     #[cfg(feature = "nix_store")]
-    build_bridge();
+    nix_store::build_bridge();
 }
 
 #[cfg(feature = "nix_store")]
-fn build_bridge() {
-    let nix_dep = NixDependency::detect();
+mod nix_store {
+    use cc::Build;
+    use version_compare::Version;
 
-    // Temporary workaround for issue in <https://github.com/NixOS/nix/pull/8484>
-    let hacky_include = {
-        let dir = tempfile::tempdir().expect("Failed to create temporary directory for workaround");
-        std::fs::write(dir.path().join("uds-remote-store.md"), "\"\"").unwrap();
-        dir
-    };
+    struct NixDependency {
+        version: String,
+    }
 
-    let mut build = cxx_build::bridge("src/nix_store/bindings/mod.rs");
-    build
-        .file("src/nix_store/bindings/nix.cpp")
-        .flag("-std=c++2a")
-        .flag("-O2")
-        .flag("-include")
-        .flag("nix/config.h")
-        .flag("-idirafter")
-        .flag(hacky_include.path().to_str().unwrap())
-        // In Nix 2.19+, nix/args/root.hh depends on being able to #include "args.hh" (which is in its parent directory), for some reason
-        .flag("-I")
-        .flag(concat!(env!("NIX_INCLUDE_PATH"), "/nix"));
+    impl NixDependency {
+        fn detect() -> Self {
+            let library = pkg_config::Config::new()
+                .cargo_metadata(false)
+                .atleast_version("2.4")
+                .probe("nix-main")
+                .expect("Failed to find nix-main >=2.4 through pkg-config");
 
-    nix_dep.apply_version_flags(&mut build);
+            Self {
+                version: library.version,
+            }
+        }
 
-    build.compile("nixbinding");
+        fn apply_version_flags(&self, build: &mut Build) {
+            let version = Version::from(&self.version).unwrap();
 
-    println!("cargo:rerun-if-changed=src/nix_store/bindings");
+            if version >= Version::from("2.20").unwrap() {
+                build.flag("-DATTIC_NIX_2_20");
+            }
+        }
 
-    // the -l flags must be after -lnixbinding
-    nix_dep.emit_cargo_metadata();
+        fn emit_cargo_metadata(&self) {
+            pkg_config::Config::new()
+                .atleast_version("2.4")
+                .probe("nix-store")
+                .unwrap();
+
+            pkg_config::Config::new()
+                .atleast_version("2.4")
+                .probe("nix-main")
+                .unwrap();
+        }
+    }
+
+    pub fn build_bridge() {
+        let nix_dep = NixDependency::detect();
+
+        // Temporary workaround for issue in <https://github.com/NixOS/nix/pull/8484>
+        let hacky_include = {
+            let dir =
+                tempfile::tempdir().expect("Failed to create temporary directory for workaround");
+            std::fs::write(dir.path().join("uds-remote-store.md"), "\"\"").unwrap();
+            dir
+        };
+
+        let mut build = cxx_build::bridge("src/nix_store/bindings/mod.rs");
+        build
+            .file("src/nix_store/bindings/nix.cpp")
+            .flag("-std=c++2a")
+            .flag("-O2")
+            .flag("-include")
+            .flag("nix/config.h")
+            .flag("-idirafter")
+            .flag(hacky_include.path().to_str().unwrap())
+            // In Nix 2.19+, nix/args/root.hh depends on being able to #include "args.hh" (which is in its parent directory), for some reason
+            .flag("-I")
+            .flag(concat!(env!("NIX_INCLUDE_PATH"), "/nix"));
+
+        nix_dep.apply_version_flags(&mut build);
+
+        build.compile("nixbinding");
+
+        println!("cargo:rerun-if-changed=src/nix_store/bindings");
+
+        // the -l flags must be after -lnixbinding
+        nix_dep.emit_cargo_metadata();
+    }
 }
